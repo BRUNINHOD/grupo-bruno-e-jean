969983d99aae680e6ebe87ba17bdd3fe4b4bb70c
BRUNINHOD <108809198+BRUNINHOD@users.noreply.github.com>
2024-08-07 09:05:22 -0300

    h

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-const numeroSenha = document.querySelector('.parametro-senha-texto')
-const botoes = document.querySelectorA('parametro-senha-botao')
+const numeroSenha = document.querySelector('.parametro-senha__texto')
+const botoes = document.querySelectorAll('.parametro-senha__botao')
+
+botoes[0].onclick = diminuirTamanho
+botoes[1].onclick = almentarTamanho
+
+let tamanhoSenha = 5
+numeroSenha.textContent = tamanhoSenha
+
+let maiuscula = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+console.log(maiuscula);
+
+function diminuirTamanho() {
+    if (tamanhoSenha > 1) 
+        
+    tamanhoSenha = tamanhoSenha - 1
+    numeroSenha.textContent = tamanhoSenha
+}
+
+function almentarTamanho() {
+    if (tamanhoSenha < 20) 
+    tamanhoSenha = tamanhoSenha + 1
+    numeroSenha.textContent = tamanhoSenha
+}
